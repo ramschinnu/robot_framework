0210f3f0b5562e0d214b9ddde4927b4c7a0922e0
ramschinnu <ramanavenky1994@gmail.com>
2024-03-10 22:12:58 +0530

    prasanth_interview

---
+++ iterator_module/all_methods_names.py
@@ -0,0 +1,4 @@
+import itertools
+print(dir(itertools))
+x=['accumulate', 'chain', 'combinations', 'combinations_with_replacement', 'compress', 'count', 'cycle', 'dropwhile', 'filterfalse', 'groupby', 'islice', 'pairwise', 'permutations', 'product', 'repeat', 'starmap', 'takewhile', 'tee', 'zip_longest']
+print(len(x))
